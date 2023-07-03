9458c09c143b4fd24f75d875b439eed57eeafe16
Ray <Chuang>
2023-07-03 16:09:22 +0800

    帳號功能-單元測試

--- springboot-mail/src/test/resources/schema.sql
+++ springboot-mail/src/test/resources/schema.sql
@@ -1,4 +1,5 @@
 --springboot會運行這個檔案很多次如果沒有加上if not exists 怕會一直重複創建
+--product
 CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -8,6 +9,16 @@ CREATE TABLE IF NOT EXISTS product
     price              INT          NOT NULL,
     stock              INT          NOT NULL,
     description        VARCHAR(1024),
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+    );
+
+--user
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE, --與mysql的語句有點不同
+    password           VARCHAR(256) NOT NULL,
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
     );
